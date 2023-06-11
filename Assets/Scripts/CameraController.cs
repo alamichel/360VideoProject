083cb84b10bd3edbd4b3c2ff00aa1515061ead0b
Alan Michel <alamichel@ucdavis.edu>
2023-06-11 11:53:50 -0700

    Add camera zoom functionality for video

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,19 +4,41 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private Camera _cameraObject;
-    [SerializeField] private int _cameraSpeed = 5;
+    // The speed the viewer can move the camera.
+    private const float CAMERA_SPEED = 5.0f;
+    // The most zoomed out the camera can go.
+    private const int MAX_FIELD_OF_VIEW = 60;
+    // The most zoomed in the camera can go.
+    private const int MIN_FIELD_OF_VIEW = 20;
 
-    // Update is called once per frame
+    private void Start()
+    {
+        // Set initial zoom for camera.
+        // Ref: <https://gamedevbeginner.com/how-to-zoom-a-camera-in-unity-3-methods-with-examples/#zoom_camera>
+        Camera.main.fieldOfView = MAX_FIELD_OF_VIEW;
+    }
+
     void Update()
     {
-        // Move around by dragging video with mouse
+        // Move around by clicking and dragging video with mouse.
         // Ref: <https://youtu.be/RxlQnPcOoYc> (03:53/09:37)
         if (Input.GetMouseButton(0))
         {
-            transform.RotateAround(transform.position, Vector3.down, _cameraSpeed * Input.GetAxis("Mouse X"));
-            transform.RotateAround(transform.position, transform.right, _cameraSpeed * Input.GetAxis("Mouse Y"));
+            transform.RotateAround(transform.position, Vector3.down, CAMERA_SPEED * Input.GetAxis("Mouse X"));
+            transform.RotateAround(transform.position, transform.right, CAMERA_SPEED * Input.GetAxis("Mouse Y"));
         }
+
+        // Zoom in/out of the view by scrolling.
+        Camera.main.fieldOfView += Input.mouseScrollDelta.y;
+
+        // Enforce limit on zooming out.
+        if (Camera.main.fieldOfView > MAX_FIELD_OF_VIEW)
+            Camera.main.fieldOfView = MAX_FIELD_OF_VIEW;
+
+        // Enforce limit on zooming in.
+        if (Camera.main.fieldOfView < MIN_FIELD_OF_VIEW)
+            Camera.main.fieldOfView = MIN_FIELD_OF_VIEW;
+
     }
 }
 
--- Assets/Scripts/VideoPlayerManager.cs
+++ Assets/Scripts/VideoPlayerManager.cs
@@ -11,13 +11,15 @@ public class VideoPlayerManager : MonoBehaviour
     // Ref: <https://docs.unity3d.com/ScriptReference/Video.VideoPlayer.html>
     void Start()
     {
+        var ray = new Ray(transform.position, transform.forward);
+
         var videoPlayer = mainCamera.AddComponent<UnityEngine.Video.VideoPlayer>();
 
         videoPlayer.playOnAwake = false;
         videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.CameraNearPlane;
         videoPlayer.targetCameraAlpha = 0.5f;
 
-        videoPlayer.url = videoPlayerObject.GetComponent<UnityEngine.Video.VideoPlayer>().url;
+        //videoPlayer.url = videoPlayerObject.GetComponent<UnityEngine.Video.VideoPlayer>().url;
         videoPlayer.isLooping = true;
 
         videoPlayer.Play();
