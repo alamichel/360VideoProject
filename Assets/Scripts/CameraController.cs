d1b13bcddf00d223f2c4b71008dd556df56c7081
Max Gao <57144940+Max-ZGao@users.noreply.github.com>
2023-06-11 16:04:00 -0700

    Added more camera controll

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -14,6 +14,8 @@ public class CameraController : MonoBehaviour
     private const int MIN_FIELD_OF_VIEW = 20;
     // change camera speed according to zoom in/out
     private float zoom_amendment = 1.0f;
+    // change dragging type
+    private bool isDragging = true;
 
     private void Start()
     {
@@ -30,12 +32,50 @@ public class CameraController : MonoBehaviour
         // Adjust camera speed by change zoom_amendment proportion to zoom in/out
         zoom_amendment = 1.0f - (MAX_FIELD_OF_VIEW - Camera.main.fieldOfView) / MAX_FIELD_OF_VIEW;
         
+        // If press "C" reset camera position and rotation
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            transform.SetPositionAndRotation(Vector3.zero, Quaternion.identity);
+        }
+
+        // if user press "V", change dragging type
+        if (Input.GetKeyDown(KeyCode.V))
+        {   
+            isDragging = !isDragging;
+        }
+
         // Move around by clicking and dragging video with mouse.
         // Ref: <https://youtu.be/RxlQnPcOoYc> (03:53/09:37)
-        if (Input.GetMouseButton(0))
+        if (!isDragging)
+        {
+            // Move around automatically by moving the mouse around the screen, mouse goes up, camera goes up, etc.
+            transform.RotateAround(transform.position, Vector3.down, zoom_amendment * (CAMERA_SPEED - 2f) * Input.GetAxis("Mouse X"));
+            transform.RotateAround(transform.position, transform.right, zoom_amendment * (CAMERA_SPEED - 2f) * Input.GetAxis("Mouse Y"));
+        }
+        else if (Input.GetMouseButton(0))
         {
             transform.RotateAround(transform.position, Vector3.down, zoom_amendment * CAMERA_SPEED * Input.GetAxis("Mouse X"));
             transform.RotateAround(transform.position, transform.right, zoom_amendment * CAMERA_SPEED * Input.GetAxis("Mouse Y"));
+        }
+        else
+        {   
+            // Move around by pressing WASD.
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.RotateAround(transform.position, transform.right, -zoom_amendment * (CAMERA_SPEED + 45f) * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.RotateAround(transform.position, transform.right, zoom_amendment * (CAMERA_SPEED + 45f) * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.RotateAround(transform.position, Vector3.down, zoom_amendment * (CAMERA_SPEED + 40f) * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.RotateAround(transform.position, Vector3.down, -zoom_amendment * (CAMERA_SPEED + 40f) * Time.deltaTime);
+            }
         }
 
         // Zoom in/out of the view by scrolling.
