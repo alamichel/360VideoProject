6ba9f16b5490b5a0299f5aa0aff94eb7363bd25f
Alan Michel <alamichel@ucdavis.edu>
2023-06-11 19:58:50 -0700

    Add basic scene transition logic; reorder 'Assets' directory

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -12,70 +12,62 @@ public class CameraController : MonoBehaviour
     private const int MAX_FIELD_OF_VIEW = 60;
     // The most zoomed in the camera can go.
     private const int MIN_FIELD_OF_VIEW = 20;
-    // change camera speed according to zoom in/out
-    private float zoom_amendment = 1.0f;
-    // change dragging type
+    // Change camera speed according to zoom in/out.
+    private float zoomAmendment = 1.0f;
+    // Change dragging type.
     private bool isDragging = true;
 
-    private void Start()
-    {
-        // Set initial zoom for camera.
-        // Ref: <https://gamedevbeginner.com/how-to-zoom-a-camera-in-unity-3-methods-with-examples/#zoom_camera>
-        Camera.main.fieldOfView = MAX_FIELD_OF_VIEW;
-
-        // Set initial camera position and rotation at the start.
-        transform.SetPositionAndRotation(Vector3.zero, Quaternion.identity);
-    }
+    private void Start() => ResetCameraView();
 
     void Update()
-    {   
-        // Adjust camera speed by change zoom_amendment proportion to zoom in/out
-        zoom_amendment = 1.0f - (MAX_FIELD_OF_VIEW - Camera.main.fieldOfView) / MAX_FIELD_OF_VIEW;
-        
-        // If press "C" reset camera position and rotation
+    {
+        // If press "C", reset camera position and rotation.
         if (Input.GetKeyDown(KeyCode.C))
-        {
             transform.SetPositionAndRotation(Vector3.zero, Quaternion.identity);
-        }
 
-        // if user press "V", change dragging type
+        // If user press "V", change dragging type.
         if (Input.GetKeyDown(KeyCode.V))
-        {   
             isDragging = !isDragging;
-        }
 
-        // Move around by clicking and dragging video with mouse.
-        // Ref: <https://youtu.be/RxlQnPcOoYc> (03:53/09:37)
+        // Move camera around as desired.
+        ChangeCameraView();
+    }
+
+    /// <summary>
+    /// Allows camera to rotate and zoom in/out.
+    /// </summary>
+    void ChangeCameraView()
+    {
+        // Adjust camera speed by change zoom_amendment proportion to zoom in/out.
+        zoomAmendment = 1.0f - (MAX_FIELD_OF_VIEW - Camera.main.fieldOfView) / MAX_FIELD_OF_VIEW;
+
         if (!isDragging)
         {
             // Move around automatically by moving the mouse around the screen, mouse goes up, camera goes up, etc.
-            transform.RotateAround(transform.position, Vector3.down, zoom_amendment * (CAMERA_SPEED - 2f) * Input.GetAxis("Mouse X"));
-            transform.RotateAround(transform.position, transform.right, zoom_amendment * (CAMERA_SPEED - 2f) * Input.GetAxis("Mouse Y"));
+            transform.RotateAround(transform.position, Vector3.down, zoomAmendment * (CAMERA_SPEED - 2f) * Input.GetAxis("Mouse X"));
+            transform.RotateAround(transform.position, transform.right, zoomAmendment * (CAMERA_SPEED - 2f) * Input.GetAxis("Mouse Y"));
         }
         else if (Input.GetMouseButton(0))
         {
-            transform.RotateAround(transform.position, Vector3.down, zoom_amendment * CAMERA_SPEED * Input.GetAxis("Mouse X"));
-            transform.RotateAround(transform.position, transform.right, zoom_amendment * CAMERA_SPEED * Input.GetAxis("Mouse Y"));
+            // Move around by clicking and dragging video with mouse.
+            // Ref: <https://youtu.be/RxlQnPcOoYc> (03:53/09:37)
+            transform.RotateAround(transform.position, Vector3.down, zoomAmendment * CAMERA_SPEED * Input.GetAxis("Mouse X"));
+            transform.RotateAround(transform.position, transform.right, zoomAmendment * CAMERA_SPEED * Input.GetAxis("Mouse Y"));
         }
         else
-        {   
-            // Move around by pressing WASD.
+        {
+            // Move around by pressing WASD/arrow keys.
             if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-            {
-                transform.RotateAround(transform.position, transform.right, -zoom_amendment * (CAMERA_SPEED + 45f) * Time.deltaTime);
-            }
+                transform.RotateAround(transform.position, transform.right, -zoomAmendment * (CAMERA_SPEED + 45f) * Time.deltaTime);
+
             if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-            {
-                transform.RotateAround(transform.position, transform.right, zoom_amendment * (CAMERA_SPEED + 45f) * Time.deltaTime);
-            }
+                transform.RotateAround(transform.position, transform.right, zoomAmendment * (CAMERA_SPEED + 45f) * Time.deltaTime);
+
             if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-            {
-                transform.RotateAround(transform.position, Vector3.down, zoom_amendment * (CAMERA_SPEED + 40f) * Time.deltaTime);
-            }
+                transform.RotateAround(transform.position, Vector3.down, zoomAmendment * (CAMERA_SPEED + 40f) * Time.deltaTime);
+
             if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-            {
-                transform.RotateAround(transform.position, Vector3.down, -zoom_amendment * (CAMERA_SPEED + 40f) * Time.deltaTime);
-            }
+                transform.RotateAround(transform.position, Vector3.down, -zoomAmendment * (CAMERA_SPEED + 40f) * Time.deltaTime);
         }
 
         // Zoom in/out of the view by scrolling.
@@ -88,6 +80,19 @@ public class CameraController : MonoBehaviour
         // Enforce limit on zooming in.
         if (Camera.main.fieldOfView < MIN_FIELD_OF_VIEW)
             Camera.main.fieldOfView = MIN_FIELD_OF_VIEW;
+    }
+
+    /// <summary>
+    /// Restores default camera view (i.e. facing forward and zoomed out) when called.
+    /// </summary>
+    public void ResetCameraView()
+    {
+        // Set initial zoom for camera.
+        // Ref: <https://gamedevbeginner.com/how-to-zoom-a-camera-in-unity-3-methods-with-examples/#zoom_camera>
+        Camera.main.fieldOfView = MAX_FIELD_OF_VIEW;
+
+        // Set initial camera position and rotation at the start.
+        transform.SetPositionAndRotation(Vector3.zero, Quaternion.identity);
     }
 }
 
---
+++ Assets/Scripts/SceneTransitionManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// <c>SceneTransitionManager</c> contains the basic logic for transitioning between gameplay scenes.
+/// </summary>
+public class SceneTransitionManager : MonoBehaviour
+{
+    // The animation that plays when we are switching scenes.
+    [SerializeField] private Animator transition;
+    // The delay after triggering the transition animation.
+    [SerializeField] private float transitionTime = 3.0f;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            ChangeSceneTo("TowerBridge");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            ChangeSceneTo("CrystalShowerFalls");
+        }
+    }
+
+    /// <summary>
+    /// Smoothly transitions from the current scene to the requested scene.
+    /// </summary>
+    /// <param name="sceneName">
+    /// The name of the scene that we should switch to, as listed in the project's build settings.
+    /// </param>
+    public void ChangeSceneTo(string sceneName) => StartCoroutine(LoadScene(sceneName));
+
+    /// <summary>
+    /// Loads the requested scene into the game.
+    /// </summary>
+    /// <param name="sceneName">
+    /// The name of the scene that we should switch to, as listed in the project's build settings.
+    /// </param>
+    /// <returns></returns>
+    IEnumerator LoadScene(string sceneName)
+    {
+        transition.SetTrigger("ChangeScene");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(sceneName);
+    }
+}
+
